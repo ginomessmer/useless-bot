5b3db97f9d3c679c0bd6ce2a2a068e8328e8ecd0
Gino Messmer <g.messmer@ftsoftware.eu>
2018-09-09 21:31:45 +0200

    Add experimental external HTTP runtime library

---
+++ UselessBot.HttpRuntime/Controllers/SettingsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UselessBot.HttpRuntime.Controllers
+{
+    [Route("api/settings")]
+    [ApiController]
+    public class SettingsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetSettings()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ UselessBot.HttpRuntime/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UselessBot.HttpRuntime
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Add services
+            // services.AddSingleton<IService, Service>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            // app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
